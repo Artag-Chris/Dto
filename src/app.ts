3f2c5c7239e239f2896465e774c006a9510903ae
Artag/Chris <scristxyz@gmail.com>
2024-08-24 21:14:52 -0500

    terminando

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,28 @@
-import { envs } from './config/envs';
-import { Server } from './presentation/server';
+import { PrismaClient } from "@prisma/client";
+import { envs } from "./config/envs";
+import { Server } from "./presentation/server";
 
-
-
-
-(async()=> {
+(async () => {
   main();
 })();
 
-
-function main() {
-
+async function main() {
   const server = new Server({
     port: envs.PORT,
     public_path: envs.PUBLIC_PATH,
   });
+ const prisma = new PrismaClient();
 
-  server.start();
+/* const newLog = await prisma.logModel.create({
+  data: {
+    level: "HIGH",
+    message: "hello world",
+    origin: "app.ts",
+  }
+}); */
+const log = await prisma.logModel.findMany();
+
+console.log({log});
+
+  //server.start();
 }
